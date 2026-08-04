6ec16c7ea897cfcc279e4c17413f0090c3cb7be3
willlliammmsmiithh <81@felix-au.me>
2026-08-04 19:02:57 +0530

    Update generator.py - Step 10

--- generator.py
+++ generator.py
@@ -1,2 +1,4 @@
 import random, string
 def gen_pass(length=12):
+    chars = string.ascii_letters + string.digits + string.punctuation
+    return ''.join(random.choice(chars) for _ in range(length))
